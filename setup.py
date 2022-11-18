6d2ababec2bd4dc40914e7bb0995a068240d8c4f
gltunnel3 <glemison.dutra09@gmail.com>
2022-11-18 04:24:34 -0300

    up

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ VERSION = __version__
 DESCRIPTION = open('README.md').read()
 AUTHOR = __author__
 AUTHOR_EMAIL = __email__
-URL = 'https://github.com/DuTra01/CheckUser'
+URL = 'https://github.com/DTunnel0/DTCheckUser'
 LICENSE = 'MIT'
 
 setup(
