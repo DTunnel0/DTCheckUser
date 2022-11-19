050ae035f3be76b96b40ab70eac1ac92ddd87462
gltunnel3 <glemison.dutra09@gmail.com>
2022-11-19 16:35:13 -0300

    up

--- checkuser/infra/ws/socketio.py
+++ checkuser/infra/ws/socketio.py
@@ -1,4 +1,7 @@
+# type: ignore
+
 import logging
+import pprint
 
 from flask import request
 from flask_socketio import SocketIO, emit
@@ -8,11 +11,44 @@ from checkuser.infra.adapter import WebSocketAdapter
 socketio = SocketIO(cors_allowed_origins='*')
 logger = logging.getLogger(__name__)
 
+connections = {}
+
 
 @socketio.on('message')
-def handle_message(data: dict):
+def on_message(data: dict) -> None:
     logger.info('IP: %s', request.remote_addr)
-    logger.info('SID: %s', request.sid)  # type: ignore
+    logger.info('SID: %s', request.sid)
 
     response = WebSocketAdapter.adapt(Controllers.get(data['action']), data['data'])
     emit('message', response)
+
+
+@socketio.on('limiter')
+def on_limiter(data: dict) -> None:
+    logger.info('-' * 50)
+    logger.info('[CONNECTED] IP: %s', request.remote_addr)
+    logger.info('[CONNECTED] SID: %s', request.sid)
+    logger.info('-' * 50)
+
+    username = data['data']['username']
+    if username in connections:
+        emit('message', {'status': 'error', 'message': 'Limite de conexões atingido'})
+        return
+
+    connections[username] = request.sid
+    emit('message', {'status': 'success', 'message': 'Conexão realizada com sucesso'})
+
+
+@socketio.on('disconnect')
+def on_disconnect() -> None:
+    for username, sid in connections.items():
+        if sid == request.sid:
+            del connections[username]
+            break
+
+    logger.info('-' * 50)
+    logger.info('[DISCONNECT] IP: %s', request.remote_addr)
+    logger.info('[DISCONNECT] SID: %s', request.sid)
+    logger.info('-' * 50)
+
+    emit('message', {'status': 'success', 'message': 'Desconectado com sucesso'})
